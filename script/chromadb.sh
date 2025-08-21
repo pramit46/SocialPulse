4fc08d7093109a0b6c183ef198c7911dcd4a88a0
pramit46 <pramit46@yahoo.com>
2025-08-21 17:58:37 +0000

    Correction in chroma script

--- script/chromadb.sh
+++ script/chromadb.sh
@@ -1,5 +1,5 @@
 pip install chromadb
-#client = chromadb.PersistentClient("shared/chroma_db")
+#client = chromadb.PersistentClient("shared/chroma-db")
 #collection = client.get_or_create_collection("bangalore_airport_social_events")
 #collection.get()
 chroma run --path shared/chroma-db
