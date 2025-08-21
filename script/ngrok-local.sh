1bf9e12fb2775594be5b0e722cfd5fe1e45fb75d
pramit46 <pramit46@yahoo.com>
2025-08-21 08:29:44 +0000

    Added ngrok & Ollama script

---
+++ script/ngrok-local.sh
@@ -0,0 +1,5 @@
+cd "C:\Users\PramitBasu\OneDrive - kyndryl\DownloadX\Softwares\ngrok-stable-windows-amd64"
+
+.\ngrok http 11434
+
+#take the Forwarding url and put in the .env (VSCode) or Accounts Secrets (Replit)
