24993aa4e5d9b14fa65b71de9c6d56e7fbd4655b
pramit46 <pramit46@yahoo.com>
2025-09-12 09:08:06 +0000

    fix for @shared

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
+      "@shared": path.resolve(__dirname, "./shared"),
     },
   },
   server: {
