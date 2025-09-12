5f2790af096418205d1fd4a6c012cb8bf4d18611
pramit46 <pramit46@yahoo.com>
2025-09-12 10:31:29 +0000

    fix for import issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: "client",
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     sourcemap: false,
     emptyOutDir: true,
   },
