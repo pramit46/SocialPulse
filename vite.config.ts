781b7f5a3f716ea673ea140421d449fe6c3932f9
pramit46 <pramit46@yahoo.com>
2025-09-12 09:05:04 +0000

    fix the error for not able to find the index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: "client",
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     sourcemap: false,
     emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
-    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./client/src"),
     },
   },
   server: {
