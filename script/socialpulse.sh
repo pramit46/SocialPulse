184960c69be898f5cff7175f2fa0faec683a70cb
pramit46 <pramit46@yahoo.com>
2025-09-24 09:13:53 +0000

    streamlined options for all the airports

--- script/socialpulse.sh
+++ script/socialpulse.sh
@@ -1,6 +1,12 @@
+# PRODUCTION DEPLOYMENT SCRIPT
 cd /workspaces/SocialPulse
 npm install
 npm run build
 # npm start is Production mode. for development mode use npm run dev
 npm start
 $BROWSER http://localhost:5000
+
+
+
+# DEVELOPMENT DEPLOYMENT SCRIPT
+npm run dev
